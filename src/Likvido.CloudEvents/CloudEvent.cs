17889b0e765f1d48df8d0d5236a1e054882b9189
Søren Pedersen <sh.pedersen@gmail.com>
2023-11-22 11:01:23 +0100

    Create initial version

---
+++ src/Likvido.CloudEvents/CloudEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Likvido.CloudEvents
+{
+    /// <summary>
+    /// Implementation of a cloud event, using v1.0
+    /// Spec: https://github.com/cloudevents/spec/blob/v1.0/spec.md
+    /// </summary>
+    public class CloudEvent
+    {
+        public string Id { get; set; }
+        public string Source { get; set; }
+        public string Type { get; set; }
+        public DateTime? Time { get; set; }
+        public string SpecVersion { get; set; } = "1.0";
+    }
+
+    public class CloudEvent<TMessage> : CloudEvent
+    {
+        public TMessage Data { get; set; }
+    }
+}
